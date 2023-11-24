477bde4b983da900c113d0302199ccb0346d6585
Pauline Hochard <pauline.hochard@cosmian.com>
2023-11-24 17:31:00 +0100

    pr review

---
+++ resources/post-install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+set -e 
+
+if [ "$EUID" -ne 0 ]; then 
+    echo "Please run as root"
+    exit 1
+fi
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <domain_name>"
+    echo -e "\nComplete the installation of the cosmian vm. Including:"
+    echo -e "\t- TLS certificate generation"
+    echo -e "\t- Nginx configuration adaptation and reloading"
+    exit 1
+fi
+
+DN_PLACEHOLDER="COSMIAN_VM_DN_PLACEHOLDER"
+DN=$1
+NGINX_CONF_PATH="/etc/nginx/sites-available/cosmian_vm_agent.conf"
+
+# Configure TLS and Nginx
+systemctl stop nginx 
+certbot certonly --standalone -d "$DN" -m  tech@cosmian.com -n --agree-tos
+sed -i "s/$DN_PLACEHOLDER/$DN/g" "$NGINX_CONF_PATH"
+ln -s "$NGINX_CONF_PATH" /etc/nginx/sites-enabled/cosmian_vm_agent.conf
+echo '0 12 * * * certbot renew --nginx --post-hook "service nginx restart"' | crontab -
+systemctl enable nginx
+systemctl start nginx
