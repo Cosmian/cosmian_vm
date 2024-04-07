dec4aed9a8012684a0b382c5e21b1561e5427d14
Emmanuel Coste <emmanuel.coste@cosmian.com>
2024-04-07 19:58:13 +0200

    ci: add luks mount option

---
+++ packer/kms-gcp-sev-rhel.pkr.hcl
@@ -0,0 +1,94 @@
+variable "prefix" {
+  type    = string
+  default = "alpha"
+}
+
+locals {
+  redhat_ami_name = "${var.prefix}-kms-rhel-sev"
+}
+
+variable "project_id" {
+  type    = string
+  default = "cosmian-dev"
+}
+
+variable "zone" {
+  type    = string
+  default = "europe-west4-a"
+}
+
+variable "ssh_username" {
+  type    = string
+  default = "root"
+}
+
+variable "ssh_timeout" {
+  type    = string
+  default = "10m"
+}
+
+variable "image_guest_os_features" {
+  type    = list(string)
+  default = ["SEV_SNP_CAPABLE"]
+}
+
+variable "network" {
+  type    = string
+  default = "default"
+}
+
+variable "subnetwork" {
+  type    = string
+  default = "default"
+}
+
+variable "tags" {
+  type    = list(string)
+  default = ["ssh"]
+}
+
+variable "use_os_login" {
+  type    = bool
+  default = true
+}
+
+variable "wait_to_add_ssh_keys" {
+  type    = string
+  default = "20s"
+}
+
+variable "redhat_source_image" {
+  type    = string
+  default = "rhel-9-v20240312"
+}
+
+variable "redhat_source_image_family" {
+  type    = string
+  default = "rhel-9"
+}
+
+source "googlecompute" "redhat" {
+  project_id              = var.project_id
+  source_image            = var.redhat_source_image
+  source_image_family     = var.redhat_source_image_family
+  zone                    = var.zone
+  ssh_username            = var.ssh_username
+  ssh_timeout             = var.ssh_timeout
+  image_name              = local.redhat_ami_name
+  image_guest_os_features = var.image_guest_os_features
+  network                 = var.network
+  subnetwork              = var.subnetwork
+  tags                    = var.tags
+  use_os_login            = var.use_os_login
+  wait_to_add_ssh_keys    = var.wait_to_add_ssh_keys
+}
+
+build {
+  sources = ["sources.googlecompute.redhat"]
+
+  provisioner "ansible" {
+    playbook_file = "../ansible/packer_sev_playbook.yml"
+    local_port    = 22
+    use_proxy     = false
+  }
+}
---
+++ resources/scripts/tests/kms-gcp_image.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+set -exu
+
+MODE=$1
+CI_INSTANCE=$2
+ZONE=$3
+IP_ADDR=$4
+GCP_DEV_PROJECT=cosmian-dev
+
+bash resources/scripts/tests/cosmian-vm-gcp_image.sh "$MODE" "$CI_INSTANCE" "$ZONE" "$IP_ADDR"
+
+echo "Checking Cosmian KMS HTTP connection..."
+curl "http://${IP_ADDR}:8080/version"
+echo ""
+echo "[ OK ] Cosmian KMS HTTP connection"
+echo "Checking Cosmian KMS HTTPS connection..."
+curl --insecure "https://${IP_ADDR}/version"
+echo ""
+echo "[ OK ] Cosmian KMS HTTPS connection"
+echo "Checking Cosmian KMS HTTP to HTTPS redirect connection..."
+curl --insecure "http://${IP_ADDR}/version"
+echo ""
+echo "[ OK ] Cosmian KMS HTTP to HTTPS redirect connection"
+
+echo "Rebooting instance..."
+gcloud beta compute instances stop "$CI_INSTANCE" --zone europe-west4-a --project "$GCP_DEV_PROJECT"
+gcloud beta compute instances start "$CI_INSTANCE" --zone europe-west4-a --project "$GCP_DEV_PROJECT"
+IP_ADDR=$(gcloud beta compute instances describe "$CI_INSTANCE" --format='get(networkInterfaces[0].accessConfigs[0].natIP)' --zone=europe-west4-a)
+timeout 4m bash -c "until curl --insecure --output /dev/null --silent --fail https://${IP_ADDR}:5355/ima/ascii; do sleep 3; done"
+
+echo "[ OK ] Cosmian VM ready after reboot"
+RESET_COUNT=$(cat cosmian_vm.snapshot | jq '.tpm_policy.reset_count')
+NEW_RESET_COUNT=$(expr $RESET_COUNT + 1)
+jq --arg NEW_RESET_COUNT "$NEW_RESET_COUNT" '.tpm_policy.reset_count = $NEW_RESET_COUNT' cosmian_vm.snapshot >new_cosmian_vm.snapshot
+jq '.tpm_policy.reset_count |= tonumber' new_cosmian_vm.snapshot | sponge new_cosmian_vm.snapshot
+./cosmian_vm --url "https://${IP_ADDR}:5355" --allow-insecure-tls verify --snapshot new_cosmian_vm.snapshot
+echo "[ OK ] Integrity after reboot"
+
+echo "Starting the KMS"
+./cosmian_vm --url "https://${IP_ADDR}:5355" --allow-insecure-tls app restart
+
+echo "[ OK ] KMS is started"
+echo "Checking Cosmian KMS HTTP connection..."
+curl "http://${IP_ADDR}:8080/version"
+echo ""
+
+echo "[ OK ] Cosmian KMS HTTP connection"
+echo "Checking Cosmian KMS HTTPS connection..."
+curl --insecure "https://${IP_ADDR}/version"
+echo ""
+
+echo "[ OK ] Cosmian KMS HTTPS connection"
+echo "Checking Cosmian KMS HTTP to HTTPS redirect connection..."
+curl --insecure "http://${IP_ADDR}/version"
+echo ""
+
+echo "[ OK ] Cosmian KMS HTTP to HTTPS redirect connection"
