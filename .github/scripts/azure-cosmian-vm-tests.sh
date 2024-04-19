a361d94391a95563090df66b2c6db8b811339453
Emmanuel Coste <emmanuel.coste@cosmian.com>
2024-04-19 06:17:14 +0200

    fix: change default port from 5355 to 5555

--- .github/scripts/azure-cosmian-vm-tests.sh
+++ .github/scripts/azure-cosmian-vm-tests.sh
@@ -7,12 +7,12 @@ IP_ADDR=$2
 
 sudo apt-get install -y jq moreutils
 
-echo "Waiting for Cosmian VM agent (${IP_ADDR}:5355)..."
-timeout 8m bash -c "until curl --insecure --output /dev/null --silent --fail https://${IP_ADDR}:5355/ima/ascii; do sleep 3; done"
+echo "Waiting for Cosmian VM agent (${IP_ADDR}:5555)..."
+timeout 8m bash -c "until curl --insecure --output /dev/null --silent --fail https://${IP_ADDR}:5555/ima/ascii; do sleep 3; done"
 
 echo "[ OK ] Cosmian VM ready"
-./cosmian_vm --url "https://${IP_ADDR}:5355" --allow-insecure-tls snapshot
-./cosmian_vm --url "https://${IP_ADDR}:5355" --allow-insecure-tls verify --snapshot cosmian_vm.snapshot
+./cosmian_vm --url "https://${IP_ADDR}:5555" --allow-insecure-tls snapshot
+./cosmian_vm --url "https://${IP_ADDR}:5555" --allow-insecure-tls verify --snapshot cosmian_vm.snapshot
 
 echo "Rebooting instance..."
 az vm restart -g "$RESOURCE_GROUP" -n "$CI_INSTANCE"
@@ -21,7 +21,7 @@ sleep 30
 
 IP_ADDR=$(az vm show -d -g "$RESOURCE_GROUP" -n "$CI_INSTANCE" --query publicIps -o tsv)
 
-timeout 8m bash -c "until curl --insecure --output /dev/null --silent --fail https://${IP_ADDR}:5355/ima/ascii; do sleep 3; done"
+timeout 8m bash -c "until curl --insecure --output /dev/null --silent --fail https://${IP_ADDR}:5555/ima/ascii; do sleep 3; done"
 
 echo "[ OK ] Cosmian VM ready after reboot"
 RESET_COUNT=$(cat cosmian_vm.snapshot | jq '.tpm_policy.reset_count')
@@ -29,5 +29,5 @@ NEW_RESET_COUNT=$(expr $RESET_COUNT + 1)
 jq --arg NEW_RESET_COUNT "$NEW_RESET_COUNT" '.tpm_policy.reset_count = $NEW_RESET_COUNT' cosmian_vm.snapshot >new_cosmian_vm.snapshot
 jq '.tpm_policy.reset_count |= tonumber' new_cosmian_vm.snapshot | sponge new_cosmian_vm.snapshot
 
-./cosmian_vm --url "https://${IP_ADDR}:5355" --allow-insecure-tls verify --snapshot new_cosmian_vm.snapshot
+./cosmian_vm --url "https://${IP_ADDR}:5555" --allow-insecure-tls verify --snapshot new_cosmian_vm.snapshot
 echo "[ OK ] Integrity after reboot"
