80511fa320ee1b838338ccdb3598fac0ed8ec654
Emmanuel Coste <emmanuel.coste@cosmian.com>
2024-05-23 17:18:30 +0200

    fix(ansible): wait after SSH service to be running

--- .github/scripts/aws-kms-tests.sh
+++ .github/scripts/aws-kms-tests.sh
@@ -16,7 +16,7 @@ echo "Cosmian VM app init"
 ./cosmian_vm --url "https://${IP_ADDR}:5555" --allow-insecure-tls app init -c ansible/roles/start_kms/templates/kms.toml.j2
 
 echo "Checking Cosmian KMS HTTP connection..."
-curl "http://${IP_ADDR}:8080/version"
+timeout 1m bash -c "until curl http://${IP_ADDR}:8080/version; do sleep 3; done"
 echo ""
 
 echo "[ OK ] Cosmian KMS HTTP connection"
