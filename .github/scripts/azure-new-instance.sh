c7864d2714a53e452a43518985cc82300d2722b3
Manuthor <32013169+Manuthor@users.noreply.github.com>
2024-06-10 20:29:35 +0200

    test(azure): freeze base-image changes (#135)
    * test(azure): freeze base-image changes
    
    * chore: revert changes

--- .github/scripts/azure-new-instance.sh
+++ .github/scripts/azure-new-instance.sh
@@ -30,6 +30,7 @@ if [ "$TECHNO" = "tdx" ]; then
     --os-disk-delete-option delete \
     --nic-delete-option delete \
     --data-disk-delete-option delete \
+    --security-type ConfidentialVM \
     --ssh-key-values "$SSH_PUB_KEY"
 else
   IMAGE_NAME="/subscriptions/e04f52be-d51f-43fe-95f8-d63a8fc91464/resourceGroups/packer-snp/providers/Microsoft.Compute/galleries/cosmian_packer/images/cosmian-vm-${DISTRIB}-${TECHNO}/versions/0.0.0"
@@ -53,6 +54,7 @@ else
     --os-disk-delete-option delete \
     --data-disk-delete-option delete \
     --admin-username azureuser \
+    --security-type ConfidentialVM \
     --ssh-key-values "$SSH_PUB_KEY"
 
 fi
