81bbdfedb3ec6a3bce7697d0403f4aa8145628d4
Emmanuel Coste <emmanuel.coste@cosmian.com>
2024-06-28 15:40:50 +0200

    fix: artifacts paths when pushing to package.cosmian.com

---
+++ .github/scripts/push_packages.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -ex
+
+set -x
+if [[ ${GITHUB_REF} = *'refs/tags/'* ]]; then
+  export BRANCH="${GITHUB_REF_NAME}"
+else
+  export BRANCH="last_build/${GITHUB_HEAD_REF:-${GITHUB_REF#refs/heads/}}"
+fi
+
+DESTINATION_DIR=/mnt/package/cosmian_vm/${BRANCH}
+ssh -o 'StrictHostKeyChecking no' -i /root/.ssh/id_rsa cosmian@package.cosmian.com mkdir -p "$DESTINATION_DIR/${DISTRIB}/"
+ssh -o 'StrictHostKeyChecking no' -i /root/.ssh/id_rsa cosmian@package.cosmian.com mkdir -p "$DESTINATION_DIR/rhel9/"
+
+ARTIFACT_NAME=cosmian_vm_${DISTRIB}
+
+scp -o 'StrictHostKeyChecking no' -i /root/.ssh/id_rsa \
+  "./$ARTIFACT_NAME/target/release/cosmian_vm_agent" \
+  "./$ARTIFACT_NAME/target/release/cosmian_vm" \
+  "./$ARTIFACT_NAME/target/release/cosmian_certtool" \
+  "./$ARTIFACT_NAME/lib/x86_64-linux-gnu/libtdx_attest.so" \
+  "./$ARTIFACT_NAME/lib/x86_64-linux-gnu/libtdx_attest.so.1" \
+  "./$ARTIFACT_NAME/lib/x86_64-linux-gnu/libtdx_attest.so.1.21.100.3" \
+  cosmian@package.cosmian.com:"$DESTINATION_DIR/${DISTRIB}/"
+
+if [[ "${DISTRIB}" = *'ubuntu-22'* ]]; then
+  scp -o 'StrictHostKeyChecking no' -i /root/.ssh/id_rsa \
+    "./$ARTIFACT_NAME/target/debian/cosmian-vm-agent_*-1_amd64.deb" \
+    "./$ARTIFACT_NAME/target/debian/cosmian-vm_*-1_amd64.deb" \
+    cosmian@package.cosmian.com:"$DESTINATION_DIR/${DISTRIB}/"
+  scp -o 'StrictHostKeyChecking no' -i /root/.ssh/id_rsa \
+    "./$ARTIFACT_NAME/target/generate-rpm/cosmian_vm-*-1.x86_64.rpm" \
+    "./$ARTIFACT_NAME/target/generate-rpm/cosmian_vm_agent-*-1.x86_64.rpm" \
+    cosmian@package.cosmian.com:"$DESTINATION_DIR/rhel9/"
+else
+  scp -o 'StrictHostKeyChecking no' -i /root/.ssh/id_rsa \
+    "./$ARTIFACT_NAME/target/debian/cosmian-vm_*-1_amd64.deb" \
+    "./$ARTIFACT_NAME/target/debian/cosmian-vm-agent_*-1_amd64.deb" \
+    cosmian@package.cosmian.com:"$DESTINATION_DIR/${DISTRIB}/"
+fi
+
+rm -rf cosmian_vm_ubuntu* target
