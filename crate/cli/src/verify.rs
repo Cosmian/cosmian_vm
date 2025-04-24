5e60934d26007bdba70427da14b0d07acf15058a
grydz <grydz@pm.me>
2025-04-24 14:25:49 +0400

    Fix: fetch IMA again when PCRs hash digest does not match the one in TPM quote

--- crate/cli/src/verify.rs
+++ crate/cli/src/verify.rs
@@ -6,13 +6,14 @@ use cosmian_vm_client::{
     snapshot::CosmianVmSnapshot,
 };
 use rand::RngCore;
+use sha2::Digest;
 use std::{fs, path::PathBuf};
 use tee_attestation::{
     az_verify_quote as az_tee_verify_quote, forge_report_data_with_nonce,
     verify_quote as tee_verify_quote,
 };
 use tokio::task::spawn_blocking;
-use tpm_quote::verify_quote as tpm_verify_quote;
+use tpm_quote::{get_pcr_digest_from_quote, verify_quote as tpm_verify_quote};
 
 /// Verify a Cosmian VM
 #[derive(Args, Debug)]
@@ -57,19 +58,45 @@ impl VerifyArgs {
                 println!("[ SKIP ] Verifying TPM attestation");
             }
             Some(filehashes) => {
-                let ima_binary = client.ima_binary().await?;
-                let tpm_quote_response = client.tpm_quote(&nonce).await?;
+                let mut ima_binary = client.ima_binary().await?;
+                let mut ima_entries = ima::ima::Ima::try_from(&ima_binary[..])?;
+
+                let mut tpm_quote_response = client.tpm_quote(&nonce).await?;
+                let mut quote_pcrs_digest = get_pcr_digest_from_quote(&tpm_quote_response.quote)?;
 
                 tracing::debug!(
                     "Cosmian VM CLI: verify: tpm_quote_response: {tpm_quote_response:?}"
                 );
 
+                let mut ima_pcr_value =
+                    ima_entries.pcr_value(tpm_quote_response.pcr_value_hash_method.clone())?;
+
+                // Try to fetch IMA at most 3 times if PCRs digest doesn't match
+                for i in 0..3 {
+                    let ima_pcr_digest = sha2::Sha256::digest(&ima_pcr_value).to_vec();
+
+                    if ima_pcr_digest == quote_pcrs_digest {
+                        break;
+                    }
+
+                    tracing::debug!(
+                        "[{}] PCR-10 digest from IMA: {:?}\nPCR-10 digest signed by TPM: {:?}, PCR",
+                        i + 1,
+                        ima_pcr_digest,
+                        quote_pcrs_digest
+                    );
+
+                    tpm_quote_response = client.tpm_quote(&nonce).await?;
+                    quote_pcrs_digest = get_pcr_digest_from_quote(&tpm_quote_response.quote)?;
+                    ima_binary = client.ima_binary().await?;
+                    ima_entries = ima::ima::Ima::try_from(&ima_binary[..])?;
+                    ima_pcr_value =
+                        ima_entries.pcr_value(tpm_quote_response.pcr_value_hash_method.clone())?;
+                }
+
                 if ima_binary.is_empty() {
                     anyhow::bail!("No IMA list recovered");
                 }
-
-                let ima_binary: &[u8] = ima_binary.as_ref();
-                let ima_entries = ima::ima::Ima::try_from(ima_binary)?;
 
                 let failures = ima_entries.compare(&filehashes.0);
                 if !failures.entries.is_empty() {
@@ -94,7 +121,7 @@ impl VerifyArgs {
                     &tpm_quote_response.signature,
                     &tpm_quote_response.public_key,
                     Some(&nonce),
-                    &ima_entries.pcr_value(tpm_quote_response.pcr_value_hash_method)?,
+                    &ima_pcr_value,
                     &snapshot
                         .tpm_policy
                         .ok_or_else(|| anyhow::anyhow!("TPM policy is missing in the snapshot"))?,
